86cc423c66d460e22643fcfb4dc54bcecf8af23f
Soumya <soumya@soumyadsanyal.com>
2016-08-17 01:01:28 -0400

    cleaned up using parser type

--- SafeParse.hs
+++ SafeParse.hs
@@ -4,70 +4,73 @@ import Data.Char
 data Try a = Fail | Succeed a
 	deriving (Show, Eq, Read, Ord)
 
-type Parser a = [a] -> [(a, [a])]
+type Parser a = [a] -> Try (a, [a])
+
+type ParseValues a = [a]
+
 
 -- need three kinds of parsers: (1) that returns whatever its argument is and doesn't touch the string to be parsed, (2) that fails on any input and returns no result, (3) that bites off the first character and returns the character with the tail 
 --
 
-safeReturnParse :: a -> [a] -> Try (a, [a])
+safeReturnParse :: a -> Parser a
 safeReturnParse = \c ->
 	\s ->
 		Succeed (c,s)
 
-safeFailParse :: [a] -> Try (a, [a])
+safeFailParse :: Parser a
 safeFailParse = \inp ->
 	Fail
 
-safeBiteParse :: [a] -> Try (a, [a])
+safeBiteParse :: Parser a
 safeBiteParse = \inp ->
 	case inp of
 		[] -> Fail
 		(x:xs) -> Succeed (x, xs)
 
-safeParseEval :: ([a] -> Try (a, [a])) -> [a] -> Try (a, [a])
+safeParseEval :: (Parser a) -> Parser a
 safeParseEval p input = p input 
 
 -- want to sequence parsers
 --
 
-safeBind :: ([a] -> Try (a, [a])) -> (a -> ([a] -> Try (a, [a]))) -> ([a] -> Try (a, [a]))
+safeBind :: (Parser a) -> (a -> (Parser a)) -> (Parser a)
 safeBind p f = \input -> case safeParseEval p input of
 	Fail -> Fail
 	Succeed (result, residue) -> safeParseEval (f result) residue 
 
-safeTry :: ([a] -> Try (a, [a])) -> ([a] -> Try (a, [a])) -> ([a] -> Try (a, [a])) 
+safeTry :: (Parser a) -> (Parser a) -> (Parser a) 
 safeTry p q = \inp -> case safeParseEval p inp of
 	Fail -> safeParseEval q inp
-	return -> return
+	returned -> returned
 
-safePredParse :: (a -> Bool) -> [a] -> Try (a, [a])
+safePredParse :: (a -> Bool) -> Parser a
 safePredParse = \p ->
 	\inp ->
-		case safeBiteParse inp of 
+		case safeParseEval safeBiteParse inp of 
 			Fail -> Fail
 			Succeed (x,xs) -> if p x then Succeed (x,xs) else Fail
 
-safeDigit :: String -> Try (Char, String)
+safeDigit :: Parser Char
 safeDigit = safePredParse isDigit
 
-safeLower :: String -> Try (Char, String)
+safeLower :: Parser Char
 safeLower = safePredParse isLower
 
-safeUpper :: String -> Try (Char, String)
+safeUpper :: Parser Char
 safeUpper = safePredParse isUpper
 
-safeLetter :: String -> Try (Char, String)
+safeLetter :: Parser Char
 safeLetter = safePredParse isAlpha
 
-safeAlphanum :: String -> Try (Char, String)
+safeAlphanum :: Parser Char
 safeAlphanum = safePredParse isAlphaNum
 
-safeChar :: Char -> String -> Try (Char, String)
+safeChar :: Char -> Parser Char
 safeChar = \c -> safePredParse ( == c)
 
 -- let's try to write our own do blocks
 
-safeDoHelper :: [([a] -> Try (a, [a]))] -> [a] -> ([a] -> b) -> ([a] -> b)
+safeDoHelper :: [(Parser a)] -> ParseValues a -> ((ParseValues a) -> b) -> ((ParseValues a) -> b)
 safeDoHelper plist store reducer = \inp -> 
 	case (plist, store) of
 		([], store) -> reducer $ reverse store
@@ -77,8 +80,7 @@ safeDoHelper plist store reducer = \inp ->
 				Succeed (result, residue) -> ((result:store), residue)
 
 	
-safeDo :: [([a] -> Try (a, [a]))] -> ([a] -> b) -> ([a] -> b) 
+safeDo :: [(Parser a)] -> ((ParseValues a) -> b) -> ((ParseValues a) -> b) 
 safeDo plist reducer = safeDoHelper plist [] reducer
 
--- next we should refactor this by using the parser type to clean the code up
 
